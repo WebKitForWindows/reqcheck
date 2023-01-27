3ad0f0f9784ee97b166a8d48dcd47a95e86bb7b2
Don <don.j.olmstead@gmail.com>
2023-01-27 14:20:54 -0800

    feat(): Add Rx stream of releases

--- go.mod
+++ go.mod
@@ -5,16 +5,25 @@ go 1.19
 require (
 	github.com/Masterminds/semver v1.5.0
 	github.com/google/go-github/v50 v50.0.0
+	github.com/reactivex/rxgo/v2 v2.5.0
 	github.com/sirupsen/logrus v1.9.0
 	golang.org/x/oauth2 v0.4.0
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/teivah/onecontext v0.0.0-20200513185103-40f981bfd775 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ releases.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2023, the WebKit for Windows project authors.  Please see the
+// AUTHORS file for details. All rights reserved. Use of this source code is
+// governed by a BSD-style license that can be found in the LICENSE file.
+
+package reqcheck
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/reactivex/rxgo/v2"
+)
+
+type ListReleaseOptions struct {
+	Owner string
+	Repo string
+	UseTags bool
+	LimitTo int
+}
+
+const (
+	startingPage = 1
+	perPageDefault = 30
+	limitToDefault = 100000000
+)
+
+func ListReleases(client Client, opts ListReleaseOptions) rxgo.Observable {
+	listOpts := ListOptions{Page: startingPage, PerPage: perPageDefault}
+
+	var listFunc func(context.Context, string, string, ListOptions) ([]Release, error)
+	if opts.UseTags {
+		listFunc = client.ListTags
+	} else {
+		listFunc = client.ListReleases
+	}
+
+	// Default limit to an arbitrarily high number
+	if opts.LimitTo == 0 {
+		opts.LimitTo = limitToDefault
+	}
+
+	itemCount := 0
+
+	return rxgo.Create([]rxgo.Producer{
+		func(ctx context.Context, next chan<- rxgo.Item) {
+			for {
+				items, err := listFunc(ctx, opts.Owner, opts.Repo, listOpts)
+				if err != nil {
+					next <- rxgo.Error(fmt.Errorf("could not access %s/%s releases: %w", opts.Owner, opts.Repo, err))
+
+					break
+				}
+
+				for _, item := range items {
+					next <- rxgo.Of(item)
+					
+					itemCount++
+					if itemCount >= opts.LimitTo {
+						break
+					}
+				}
+
+				if len(items) < listOpts.PerPage {
+					break
+				}
+
+				listOpts.Page++
+			}
+		},
+	})
+}
