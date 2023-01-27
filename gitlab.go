a7bb5f51333b77c6e2092c2289e7187a30a5c640
Don <don.j.olmstead@gmail.com>
2023-01-27 14:36:47 -0800

    feat(): Add GitLab SCM Client

---
+++ gitlab.go
@@ -0,0 +1,110 @@
+// Copyright (c) 2023, the WebKit for Windows project authors.  Please see the
+// AUTHORS file for details. All rights reserved. Use of this source code is
+// governed by a BSD-style license that can be found in the LICENSE file.
+
+package reqcheck
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/sirupsen/logrus"
+	"github.com/xanzy/go-gitlab"
+)
+
+type gitlabClient struct {
+	client *gitlab.Client
+}
+
+func NewGitLab(uri, token string) (Client, error) {
+	return NewGitLabClient(uri, token, http.DefaultClient)
+}
+
+func NewGitLabClient(uri, token string, cl *http.Client) (Client, error) {
+	// Parse the url
+	gitlabURL, err := url.Parse(uri)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse Gitlab link: %w", err)
+	}
+
+	// Get base url for API
+	relBaseURL, _ := url.Parse("./api/v4")
+	baseURL := gitlabURL.ResolveReference(relBaseURL)
+
+	logrus.WithFields(logrus.Fields{
+		"gitlab-url": gitlabURL.String(),
+		"base-url":   baseURL.String(),
+	}).Debug("connecting to gitlab instance")
+
+	// Create the client
+	client, err := gitlab.NewClient(
+		token,
+		gitlab.WithBaseURL(baseURL.String()),
+		gitlab.WithHTTPClient(cl),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to GitLab instance %s: %w", uri, err)
+	}
+
+	return &gitlabClient{client: client}, nil
+}
+
+func (c *gitlabClient) ListReleases(ctx context.Context, owner, name string, opt ListOptions) ([]Release, error) {
+	glOpts := &gitlab.ListReleasesOptions{
+		ListOptions: gitlab.ListOptions{
+			Page:    opt.Page,
+			PerPage: opt.PerPage,
+		},
+	}
+
+	releases, _, err := c.client.Releases.ListReleases(fmt.Sprintf("%s/%s", owner, name), glOpts)
+	if err != nil {
+		return nil, fmt.Errorf("error getting releases from repository %s/%s: %w", owner, name, err)
+	}
+
+	r := make([]Release, 0, glOpts.PerPage)
+
+	for _, release := range releases {
+		tagName := release.TagName
+
+		logrus.WithFields(logrus.Fields{
+			"tag":    tagName,
+			"commit": release.Commit.ID,
+		}).Debug("found release")
+
+		r = append(r, Release{Tag: tagName, SemVer: generateVersion(tagName, versionMatcher)})
+	}
+
+	return r, nil
+}
+
+func (c *gitlabClient) ListTags(ctx context.Context, owner, name string, opt ListOptions) ([]Release, error) {
+	glOpts := &gitlab.ListTagsOptions{
+		ListOptions: gitlab.ListOptions{
+			Page:    opt.Page,
+			PerPage: opt.PerPage,
+		},
+	}
+
+	tags, _, err := c.client.Tags.ListTags(fmt.Sprintf("%s/%s", owner, name), glOpts)
+	if err != nil {
+		return nil, fmt.Errorf("error getting tags from repository %s/%s: %w", owner, name, err)
+	}
+
+	r := make([]Release, 0, glOpts.PerPage)
+
+	for _, tag := range tags {
+		tagName := tag.Name
+
+		logrus.WithFields(logrus.Fields{
+			"tag":    tagName,
+			"commit": tag.Commit.ID,
+		}).Debug("found tag")
+
+		r = append(r, Release{Tag: tagName, SemVer: generateVersion(tagName, versionMatcher)})
+	}
+
+	return r, nil
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/reactivex/rxgo/v2 v2.5.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/urfave/cli/v2 v2.24.1
+	github.com/xanzy/go-gitlab v0.79.0
 	golang.org/x/oauth2 v0.4.0
 )
 
@@ -18,6 +19,8 @@ require (
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
@@ -27,7 +30,8 @@ require (
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
