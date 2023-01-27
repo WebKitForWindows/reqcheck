2b4875eb49103c4da22e99c4b125120b515a69a4
Don <don.j.olmstead@gmail.com>
2023-01-27 14:47:52 -0800

    feat(): Generalize query CLI command
    Use `reqcheck.NewClientFromDriver` to make a general purpose query command.

--- cmd/reqcheck/github.go
+++ cmd/reqcheck/github.go
@@ -4,23 +4,10 @@
 
 package main
 
-import (
-	"fmt"
-
-	"github.com/Masterminds/semver"
-	"github.com/WebKitForWindows/reqcheck"
-	"github.com/urfave/cli/v2"
-)
+import "github.com/urfave/cli/v2"
 
 func githubCmd() *cli.Command {
-	settings := struct {
-		URI        string
-		Token      string
-		Tags       bool
-		Prerelease bool
-		Constraint string
-		LimitTo    int
-	}{}
+	var settings querySettings
 
 	return &cli.Command{
 		Name:      "github",
@@ -60,57 +47,6 @@ func githubCmd() *cli.Command {
 				Destination: &settings.LimitTo,
 			},
 		},
-		Action: func(c *cli.Context) error {
-			if settings.Token == "" {
-				return fmt.Errorf("no token provided: %w", ErrCli)
-			}
-
-			if c.NArg() != 2 {
-				return fmt.Errorf("command takes two arguments <owner> <repo>: %w", ErrCli)
-			}
-
-			owner := c.Args().Get(0)
-			repo := c.Args().Get(1)
-
-			client, err := reqcheck.NewGitHub(settings.URI, settings.Token)
-			if err != nil {
-				return fmt.Errorf("could not connect to github server at %s: %w", settings.URI, err)
-			}
-
-			releaseOpts := reqcheck.ListReleaseOptions{
-				Owner:   owner,
-				Repo:    repo,
-				Tags:    settings.Tags,
-				LimitTo: settings.LimitTo,
-			}
-
-			observer := reqcheck.ListReleases(client, releaseOpts)
-
-			if settings.Constraint != "" {
-				constraint, err := semver.NewConstraint(settings.Constraint)
-				if err != nil {
-					return fmt.Errorf("could not parse constraint %s: %w", settings.Constraint, err)
-				}
-
-				observer = observer.Filter(reqcheck.FilterSemanticConstraint(constraint))
-			} else if !settings.Prerelease {
-				observer = observer.Filter(reqcheck.FilterStableRelease)
-			}
-
-			for item := range observer.Observe() {
-				if item.Error() {
-					return fmt.Errorf("error when getting releases for %s%s: %w", owner, repo, err)
-				}
-
-				release := item.V.(reqcheck.Release)
-				if release.SemVer != nil {
-					fmt.Printf("tag %s -> semver %s\n", release.Tag, release.SemVer.String())
-				} else {
-					fmt.Printf("tag %s -> semver ???\n", release.Tag)
-				}
-			}
-
-			return nil
-		},
+		Action: queryAction("github", &settings),
 	}
 }
